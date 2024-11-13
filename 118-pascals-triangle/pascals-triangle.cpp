9a7d63dcfade999a98a5954272b1e807f69b9912
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2024-11-13 01:14:38 -0600

    Time: 0 ms (100.00%) | Memory: 8.1 MB (90.87%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> res(n, vector<int> (n, 0));
+        for(int i = 1; i <= n; i++) {
+            res[i-1].resize(i);
+            res[i-1][0] = 1;
+            res[i-1][i-1] = 1;
+
+            for(int j = 1; j < i-1; j++) {
+                res[i-1][j] = res[i-2][j-1] + res[i-2][j];
+            }
+        }
+        return res;
+    }
+};
