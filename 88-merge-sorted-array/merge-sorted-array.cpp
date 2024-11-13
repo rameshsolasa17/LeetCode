39a073f382307e695d3885e28b7c8e0aec20ed8c
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2024-11-13 03:21:29 -0600

    Time: 0 ms (100.00%) | Memory: 11.4 MB (22.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,23 +1,37 @@
+//gap method
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1, j=0;
-        while(i>=0 && j<n) {
-            if(nums1[i] > nums2[j]) {
-                swap(nums1[i], nums2[j]);
-                i--;
+        int gap = (m+n)/2 + (m+n)%2;
+        while(gap) {
+            int i = 0;
+            int j = i + gap;
+
+            while(j<m) {
+                if(nums1[i] > nums1[j]) swap(nums1[i], nums1[j]);
+                i++; j++;
+            }
+
+            while(i<m && j-m<n) {
+                if(nums1[i] > nums2[j-m]) swap(nums1[i], nums2[j-m]);
+                i++;
                 j++;
             }
-            else break;
+
+            while(j-m<n) {
+                if(nums2[i-m] > nums2[j-m]) swap(nums2[i-m], nums2[j-m]);
+                i++; j++;
+            }
+
+            if(gap == 1) break;
+
+            gap = gap/2 + gap%2;
         }
 
-        sort(nums1.begin(), nums1.begin()+m);
-        sort(nums2.begin(), nums2.end());
-
-        j = 0;
-        i = m;
-        while(j<n) {
-            nums1[i++] = nums2[j++];
+        int j = 0;
+        while(j < n) {
+            nums1[m+j] = nums2[j];
+            j++;
         }
     }
 };
