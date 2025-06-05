5771b213e25c1285a3c825eea48a6b4ce033fbdd
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2025-06-04 19:47:05 -0500

    Time: 3 ms (16.98%) | Memory: 14.4 MB (9.23%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void fun(int i, int t, vector<int>& nums, vector<int> temp, vector<vector<int>>& res, int k, int n) {
+        if(t == n && temp.size() == k) {
+            res.push_back(temp);
+            return ;
+        }
+
+        if(i == nums.size()) return ;
+
+        if(t>=n) return ;
+        if(temp.size()>=k) return ;
+        fun(i+1, t, nums, temp, res, k, n);
+        t+=nums[i];
+        temp.push_back(nums[i]);
+        fun(i+1, t, nums, temp, res, k, n);
+
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        vector<vector<int>> res;
+        vector<int> temp;
+        fun(0, 0, nums, temp, res, k, n);
+        return res;
+    }
+};
