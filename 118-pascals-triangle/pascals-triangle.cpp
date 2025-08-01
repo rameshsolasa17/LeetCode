bc5d9aaf822fa5bd3faf671b0ee4966ed754bb7a
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2025-07-31 21:06:35 -0500

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.63%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
-        // 1st variation
-        vector<vector<int>> res;
-        res.push_back({1});
-        n-=1;
-
-        for(int i = 1; i <= n; i++) {
-            vector<int> temp;
-            temp.push_back(1);
-            for(int j = 1; j <= i-1; j++) {
-                temp.push_back(res[i-1][j-1] + res[i-1][j]);
-            }
-            temp.push_back(1);
-
+    vector<int> row(int n) {
+        vector<int> res;
+        int temp = 1;
+        for(int i = 0; i < n; i++) {
             res.push_back(temp);
+
+            temp*=(n-i-1);
+            temp/=(i+1);
+
         }
 
+        return res;
+    }
+    vector<vector<int>> generate(int n) {
+        // 2nd variation
+        vector<vector<int>> res;
+        for(int i = 1; i <= n; i++) {
+            res.push_back(row(i));
+        }
         return res;
 
     }
